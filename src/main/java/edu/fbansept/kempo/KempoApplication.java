a84a47b7480a2756242f8e56b2f79f641fa24711
fbansept <bansept.franck@gmail.com>
2025-01-20 11:41:22 +0100

    Initial commit

---
+++ src/main/java/edu/fbansept/kempo/KempoApplication.java
@@ -0,0 +1,13 @@
+package edu.fbansept.kempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KempoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fbansept/kempo/KempoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fbansept.kempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
